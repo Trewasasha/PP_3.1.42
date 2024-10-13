2574f439f286a6fa720150488cc464a5c503b4eb
Trewas345 <vazheninsaha@gmail.com>
2024-10-13 17:41:06 +0600

    3.1.4

---
+++ src/main/java/ru/vovk/springboot/service/UserService.java
@@ -0,0 +1,23 @@
+package ru.vovk.springboot.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import ru.vovk.springboot.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService extends UserDetailsService {
+
+    List<User> getAllUsers();
+
+    Optional<User> getUserById(Long id);
+
+    Optional<User> getUserByUsername(String username);
+
+    User saveUser(User user);
+
+    User updateUser(Long id, String username, String email, String password, List<String> roleNames);
+
+    void deleteUser(Long id);
+
+}
